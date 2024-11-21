901320f61a4eb115e8e58ed0aa572d4bcaa6a64b
0xNizar <gainonce01@gmail.com>
2024-11-21 14:18:27 +0300

    Command Template

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+export default {
+    name: "ping",
+    description: 'Execute a JavaScript code.',
+    aliases: ['p'],
+    otherOptions: {
+        botOwner: false
+    },
+
+    run: async (client, message, args) => {
+
+    }
+}
--- index.js
+++ index.js
@@ -10,12 +10,4 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
 client.login(process.env.token);
